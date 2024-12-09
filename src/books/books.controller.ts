0fbcd976f0ddb51c58e65d8a3db1706c03a1eca2
Elyas <Elyas575@hotmail.com>
2024-12-09 12:17:20 +0200

    comments added

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -8,13 +8,23 @@ import { Book } from './interfaces/books.interface';
 
 export class BooksController{
     constructor(private readonly _booksService:BooksService){}
-
-    @Get('/search') 
+      /**
+         * Route to search for books based on query parameters.
+         * The query parameters (title, author, price, category, publication date.) are validated then passed to the BooksService to fetch the books
+         * @param params - the optional query parameters for filtering the books
+         * returns An array of books matching the search criteria
+      */
+    @Get('/search')
     getBooks(@Query() params?: GetAllBooksParamsDto) : Book[]{
         const booksToFind  = this._booksService.getAllBooks(params);
         return booksToFind;   
     }
-
+    /**
+         * Route to get a specific book by its ID. the id is converted to int using the ParseIntPipe then passed to the bookService
+         * @param bookId - The ID of the book to retrieve
+         * if found returns The book with the specified ID
+  
+     */
     @Get('/:id')
     getBookById(@Param('id', ParseIntPipe) bookId:number ) : Book  {
         const bookToFind =  this._booksService.getBookById(bookId);
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -6,53 +6,57 @@ import { GetAllBooksParamsDto } from "./dtos/get-all-books-params.dto";
 
 @Injectable()
 export class BooksService {
-// to do: refactor the code later maybe use a switch case or a generic function
     books: Book[] = booksData;
+      /**
+         * Retrieves a list of books, optionally filtered by query parameters.
+         * If no parameters are provided all books are returned.
+         * Filtering is done by checking each parameter (e.g., price, category, title) and applying it's filter
+         * @param params - The optional query parameters for filtering books (price, category, author, title, date(publication date)).
+         * returns An array of books that match the filter criteria.
+      */
     getAllBooks(params?:GetAllBooksParamsDto): Book[]{
         let filteredBooks :Book[] = booksData;
 
+        // If no parameters are provided, return the full list
         if(!params){
             return filteredBooks;
         }
-
+        // Filter by price if specified in the query parameters
         if(params.price){
              filteredBooks = this.books.filter(book => book.price === Number(params.price))
         }
-
+        // Filter by category if specified in the query parameters
         if(params.category){
             filteredBooks = filteredBooks.filter(book => book.category.toLowerCase() === params.category.toLowerCase())
         }
-
+        // Filter by author if specified in the query parameters
         if(params.author){
             filteredBooks = filteredBooks.filter(book => book.author.toLowerCase() === params.author.toLowerCase())
         }
-
+        // Filter by title if specified in the query parameters
         if(params.title){
             filteredBooks = filteredBooks.filter(book => book.title.toLowerCase() === params.title.toLowerCase())
         }
      
+        // Filter by publication date if specified in the query parameters
         if(params.date){
             // check if it's a year or a full date
             const year = params.date.length === 4;
-
+           // If it's a year filter by the first 4 characters of the publication date
             if(year){
-             // Extract the year from the params.date
             filteredBooks = filteredBooks.filter(book => {
-                // Extract year from the book's publicationDate
                 const bookYear = book.publication_date.slice(0, 4);
                 return bookYear === params.date;
              });
-            } else {
+            } else { 
+                // Otherwise filter by the full publication date
                 filteredBooks = filteredBooks.filter(book => book.publication_date === params.date);
             }
         } 
         return filteredBooks;
     }
-
     getBookById(id:number): Book{
         return this.books.find(book => book.id === id);
         
     }
-
-   
 }
